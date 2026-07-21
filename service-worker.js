ac4644968734371e5050c145e0c5373ad6415519
davidev2023 <133832913+davidev2023@users.noreply.github.com>
2026-07-21 16:35:49 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,112 @@
-const cacheName = "dm-financeira-v3";
+const cacheName = "dm-financeira-v4";
+
 
 const arquivos = [
+
     "./",
     "./index.html",
     "./style.css",
-    "./app.js"
+    "./app.js",
+    "./manifest.json",
+    "./logo.png"
+
 ];
 
 
+
+// INSTALAÇÃO
+
 self.addEventListener("install", evento => {
 
+
     evento.waitUntil(
+
         caches.open(cacheName)
-        .then(cache => cache.addAll(arquivos))
-    );
 
-});
+        .then(cache => {
 
-
-self.addEventListener("fetch", evento => {
-
-    evento.respondWith(
-
-        caches.match(evento.request)
-        .then(resposta => {
-
-            return resposta || fetch(evento.request);
+            return cache.addAll(arquivos);
 
         })
 
     );
+
+
+    self.skipWaiting();
+
+
+});
+
+
+
+
+// ATIVAÇÃO
+// remove caches antigos
+
+self.addEventListener("activate", evento => {
+
+
+    evento.waitUntil(
+
+
+        caches.keys()
+
+        .then(chaves => {
+
+
+            return Promise.all(
+
+
+                chaves.map(chave => {
+
+
+                    if(chave !== cacheName){
+
+                        return caches.delete(chave);
+
+                    }
+
+
+                })
+
+
+            );
+
+
+        })
+
+
+    );
+
+
+    self.clients.claim();
+
+
+});
+
+
+
+
+// FUNCIONAMENTO OFFLINE
+
+self.addEventListener("fetch", evento => {
+
+
+    evento.respondWith(
+
+
+        caches.match(evento.request)
+
+        .then(resposta => {
+
+
+            return resposta || fetch(evento.request);
+
+
+        })
+
+
+    );
+
 
 });
