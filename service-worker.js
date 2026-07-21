6e66bfa19b48c8d1cd08d974ec9aa35822614af1
davidev2023 <133832913+davidev2023@users.noreply.github.com>
2026-07-21 15:32:56 -0300

    Integrate Firebase for client management
    Implement service worker for caching files.

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const cacheName = "dm-financeira-v1";
+
+const arquivos = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js"
+];
+
+
+self.addEventListener("install", evento => {
+
+    evento.waitUntil(
+        caches.open(cacheName)
+        .then(cache => cache.addAll(arquivos))
+    );
+
+});
+
+
+self.addEventListener("fetch", evento => {
+
+    evento.respondWith(
+
+        caches.match(evento.request)
+        .then(resposta => {
+
+            return resposta || fetch(evento.request);
+
+        })
+
+    );
+
+});
